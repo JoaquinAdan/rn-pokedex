21346fdad3adce36170ee21594dfc2b31cc4e1ad
JoaquinAdan <rodriguezjdev@gmail.com>
2024-03-25 14:59:28 -0300

    Update LoginForm component with styling and functionality

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,10 +1,41 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Button, Keyboard } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function LoginForm() {
   return (
-    <View>
-      <Text>LoginForm</Text>
-    </View>
+    <SafeAreaView>
+      <Text style={styles.title}>Iniciar sesión</Text>
+      <TextInput style={styles.input} placeholder='Usuario' autoCapitalize='none' />
+      <TextInput style={styles.input} placeholder='Contraseña' autoCapitalize='none' secureTextEntry={true} />
+      <Button
+        style={styles.button}
+        title='Entrar'
+        onPress={() => {
+          Keyboard.dismiss()
+          console.log('Enrando...')
+        }}
+      />
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: 'center',
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 15,
+    marginTop: 20,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+  button: {
+    width: '50%',
+  },
+})
