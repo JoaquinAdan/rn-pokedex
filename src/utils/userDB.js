47e558ad2c81e53ecc85b99070b6a016a0e0734a
JoaquinAdan <rodriguezjdev@gmail.com>
2024-03-25 14:51:41 -0300

    Add userDB.js file with user and userDetails objects

---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: 'loakuz',
+  password: '123456',
+}
+
+export const userDetails = {
+  username: 'loakuz',
+  firstName: 'Joaquin',
+  lastName: 'Rodriguez',
+  email: 'rodriguezjdev@gmail.com',
+}
